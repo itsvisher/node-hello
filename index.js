9f2416fd7ef0eb38ebf1996ff6e477ca45699f6d
Vishal Srivastava <android.vish@gmail.com>
2022-05-15 13:01:05 +0530

    Updated index
    Changed landing text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello folks!\nWelcome to this node js app.'
   res.end(msg);
 });
 
